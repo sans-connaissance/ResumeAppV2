ffed646d8429d4557171969a60fb509b80957c00
sans-connaissance <davemalicke@gmail.com>
2022-01-27 20:23:49 -0500

    ADD ResumeItemImageView

---
+++ ResumeApp2/Views/ResumeItemImageView.swift
@@ -0,0 +1,27 @@
+//
+//  ResumeItemImageView.swift
+//  ResumeApp2
+//
+//  Created by David Malicke on 1/27/22.
+//
+import Kingfisher
+import SwiftUI
+
+struct ResumeItemImageView: View {
+    
+    var imageString: String
+    
+    var body: some View {
+        KFImage(URL(string: imageString))
+            .resizable()
+            .scaledToFill()
+    }
+}
+
+struct ResumeItemImageView_Previews: PreviewProvider {
+    static var previews: some View {
+        if let imageString = masterDegree.thumbnail {
+            ResumeItemImageView(imageString: imageString)
+        }
+    }
+}
