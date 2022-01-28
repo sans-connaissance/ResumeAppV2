8ce5aedb037257801cc813760eaca527acba0b84
sans-connaissance <davemalicke@gmail.com>
2022-01-27 20:49:39 -0500

    ADD HomeVM init funcs and array setup

---
+++ ResumeApp2/Views/HomeView.swift
@@ -0,0 +1,37 @@
+//
+//  HomeView.swift
+//  ResumeApp2
+//
+//  Created by David Malicke on 1/27/22.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var vm = HomeVM()
+    let screen = UIScreen.main.bounds
+    
+    var body: some View {
+        NavigationView {
+            VStack {
+                // main VStack
+                ScrollView(showsIndicators: false) {
+                    // change this to VStack to remove skipping while running
+                    LazyVStack {
+                        Text(vm.educationArray[0].degreeName!)
+                        Text(vm.educationArray[1].degreeName!)
+                        Text(vm.educationArray[2].degreeName!)
+
+                    }
+                }
+            }
+            .navigationBarHidden(true)
+        }
+
+    }
+}
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
